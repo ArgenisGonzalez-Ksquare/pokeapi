84549a53152a3193ca6d61c957f0309bd83f9254
ArgenisGonzalez-Ksquare <argenis.gonzalez@itksquare.edu.mx>
2022-12-26 22:29:40 -0400

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,16 @@ import {React, useEffect, useState} from 'react';
 import Pokebox from './components/Pokebox';
 
 
+
 function App() {
-/* 
-    const pokemonList = arrPok.map((value) => <li> {value.name} </li>) */
+    const [pokeCard, setPokeCard] = useState([1,2,3,4,5]);
+    const List = pokeCard.map((value) => <Pokebox id={value} key={value}> </Pokebox>)
     return (
-    <body className="App">
-      <Pokebox id={1} />
-      <Pokebox id={2} />
-      <Pokebox id={3} />
-      <Pokebox id={4} />
-      <Pokebox id={5} />
-    </body>
+    <main className="App"> 
+    {List}
+    </main>
   );
 }
+
 
 export default App;
--- src/components/Pokebox.jsx
+++ src/components/Pokebox.jsx
@@ -1,4 +1,5 @@
-import {React, useEffect, useState} from 'react';
+import { cleanup } from '@testing-library/react';
+import {Fragment, React, useEffect, useState} from 'react';
 
 const Pokebox = (id) => {
 
@@ -9,17 +10,16 @@ const Pokebox = (id) => {
         const req = await fetch(`https://pokeapi.co/api/v2/pokemon/${number.id}`);
         const pokemonData = await req.json();
         setPokemon(pokemonData)
-        console.log(pokemonData)
       }
       fetchPokemon(id)
-
     },[setPokemon])
+
 
     
     return(
     <div className="pokemonBox">
-        <img src={pokemon.sprites.other["official-artwork"].front_default} alt="pokemon image" />
-        <h1>{pokemon.name} </h1>
+{        <img src={pokemon.sprites.other["official-artwork"].front_default} alt="pokemon image" />
+}        <h1>{pokemon.name} </h1>
         <p> {pokemon.order} - {pokemon.height}</p>
     </div>
     )
